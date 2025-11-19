ea60652080ee719a916451b3caa4f7c83b93f253
BiancaBaccin <bianca.baccin@gmail.com>
2025-11-18 23:43:01 -0300

    Adiciona classe Popup

---
+++ scripts/Popup.js
@@ -0,0 +1,39 @@
+class Popup {
+  constructor(popup) {
+    this._popup = popup;
+
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  open() {
+    this._popup.style.display = "flex";
+
+    document.addEventListener("keydown", this._handleEscClose);
+  }
+
+  close() {
+    this._popup.style.display = "none";
+
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    const closeButton = this._popup.querySelector('[class*="close-button"]');
+
+    closeButton.addEventListener("click", () => {
+      this.close();
+    });
+
+    this._popup.addEventListener("click", (evt) => {
+      if (evt.target === this._popup) {
+        this.close();
+      }
+    });
+  }
+}
