f985c4bc21473081cf73c8109f4e9f682c511ef9
BiancaBaccin <bianca.baccin@gmail.com>
2025-11-18 21:13:09 -0300

    Adiciona classe Section

---
+++ scripts/Section.js
@@ -0,0 +1,18 @@
+class Section {
+  constructor({ items, renderer }, cardElement) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(cardElement);
+  }
+
+  renderItems() {
+    this._items.forEach((item) => {
+      const element = this._renderer(item);
+      this._container.append(element);
+    });
+  }
+
+  addItem(element) {
+    this._container.append(element);
+  }
+}
